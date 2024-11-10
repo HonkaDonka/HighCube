4fcf728700daa2197149be5f953507c1be974fd3
Vinson Huang <vinsonh789@gmail.com>
2024-11-10 17:56:49 -0600

    Reformat + Temperature

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,10 @@
 #include <Adafruit_LIS3DH.h>
 #include <Adafruit_Sensor.h>
 #include <Stepper.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
+// LIS3DH (SPI)
 #define LIS3DH_CLK 18
 #define LIS3DH_MISO 19
 #define LIS3DH_MOSI 23
@@ -11,21 +14,28 @@
 
 Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS, LIS3DH_MOSI, LIS3DH_MISO, LIS3DH_CLK);
 
+// DS18B20 
+#define ONE_WIRE_BUS 15
 
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
 
-
-const int stepsPerRevolution = 2048;  // change this to fit the number of steps per revolution
-const int rolePerMinute = 15;         // Adjustable range of 28BYJ-48 stepper is 0~17 rpm
-
-// initialize the stepper library on pins 8 through 11:
-Stepper myStepper(stepsPerRevolution, 21, 2, 4, 22);
-
+// Stepper motor 
+const int stepsPerRevolution = 2048;  // Steps per full revolution for 28BYJ-48
+const int rolePerMinute = 15;         // Stepper motor speed (0 to 17 rpm)
+Stepper myStepper(stepsPerRevolution, 21, 2, 4, 22); 
 
 void setup() {
-  // put your setup code here, to run once:
+  // Serial monitor setup
   Serial.begin(9600);
-  while (!Serial) delay(10);     // will pause Zero, Leonardo, etc until serial console opens
+  while (!Serial) delay(10); 
+  
+
   SPI.begin();
+  sensors.begin();
+  myStepper.setSpeed(rolePerMinute);
+
+  // LIS3DH and stepper pins
   pinMode(LIS3DH_CLK, OUTPUT);
   pinMode(LIS3DH_MISO, INPUT);
   pinMode(LIS3DH_MOSI, OUTPUT);
@@ -36,50 +46,48 @@ void setup() {
   pinMode(4, OUTPUT);
   pinMode(22, OUTPUT);
 
-  // Serial.println("LIS3DH test!");
+  pinMode(ONE_WIRE_BUS, INPUT);
 
-  if (! lis.begin(0x18)) {   // change this to 0x19 for alternative i2c address
-    while (1) yield();
+  // Check LIS3DH connection
+  if (!lis.begin(0x18)) {
+    while (1) yield();  
   }
-
-  myStepper.setSpeed(rolePerMinute);
-  
 }
 
 void loop() {
+  // Read accelerometer data
   sensors_event_t event;
   lis.getEvent(&event);
-
-  /* Display the results (acceleration is measured in m/s^2) */
-  Serial.print("\t\tX: "); Serial.print(event.acceleration.x);
+  
+  Serial.print("X: "); Serial.print(event.acceleration.x);
   Serial.print(" \tY: "); Serial.print(event.acceleration.y);
   Serial.print(" \tZ: "); Serial.print(event.acceleration.z);
-  Serial.println(" m/s^2 ");
-
-  Serial.println("");
+  Serial.println(" m/s^2");
 
   if (event.acceleration.y > 1.0) {
-    myStepper.step(128);
-  } else if (event.acceleration.y < -1.0) {
     myStepper.step(-128);
+  } else if (event.acceleration.y < -1.0) {
+    myStepper.step(128);
   }
 
-  // myStepper.step(stepsPerRevolution);
+  // Readtemperature
+  sensors.requestTemperatures();  
 
-  // delay(200);
+  float temperatureC = sensors.getTempCByIndex(0);
+  if (temperatureC != DEVICE_DISCONNECTED_C) {
+    Serial.print("Temperature: ");
+    Serial.print(temperatureC);
+    Serial.println(" °C");
+  }
 
-  // myStepper.step(-stepsPerRevolution);
-
-  delay(10);
-
+  delay(1000);  
 }
 
-// Have a cube that changes what it displays based on the orientation
-// Accelerometer to determine ^
-// Room temperature along with weather outside
-// time/date
-// alarm?
-// cute face/anims
-
-// status APA106 leds
-// maybe use solar power? 
+// WIP:
+// - Display based on orientation
+// - Room and outside temperature
+// - Time/date display
+// - Alarm feature
+// - Cute face animations
+// - Status display using APA106 LEDs
+// - Possible solar power use
